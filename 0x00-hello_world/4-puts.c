4eaea25ec8b74e7ac6776499b2f615cf04824a89
Feliz <fndayi5@gmail.com>
2022-09-12 22:49:38 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,12 +7,8 @@
  *this program prints "Programming is like building a multilingual puzzle
  *Return: 0
  */
-
 int main(void)
-
 {
 	puts("\"Programming is like builing a multilingual puzzle");
-
-	    return (0);
-
+	return (0);
 }
