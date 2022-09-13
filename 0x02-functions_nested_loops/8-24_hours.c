5e7ea8c511892d8101f658740b42a9abd0812762
Feliz <fndayi5@gmail.com>
2022-09-13 19:33:29 +0200

    newchanges

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,26 +6,26 @@
  */
 void jack_bauer(void)
 {
-    int h = 0;
-    int min = 0;
-    int h_rem;
-    int min_rem;
+	int h = 0;
+	int min = 0;
+	int h_rem;
+	int min_rem;
 
-    while (h <= 23)
-    {
-    while (min <= 59)
-    {
-    min_rem = min % 10;
-    h_rem = h % 10;
-    _putchar(h /10 + '0');
-    _putchar(h_rem + '0');
-    _putchar(':');
-    _putchar(min / 10 + '0');
-    _putchar(min_rem + '0');
-    min++;
-    _putchar('\n');
-    }
+	while (h <= 23)
+	{
+	while (min <= 59)
+	{
+	min_rem = min % 10;
+ 	h_rem = h % 10;
+	_putchar(h /10 + '0');
+	_putchar(h_rem + '0');
+	_putchar(':');
+	_putchar(min / 10 + '0');
+	_putchar(min_rem + '0');
+	min++;
+	_putchar('\n');
+	}
 h++;
 min = 0;
-    }
+	}
 }
