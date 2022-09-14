602d29f1d217464274052c78bd15e5abf1aa1b80
Feliz <fndayi5@gmail.com>
2022-09-14 04:58:16 +0200

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,9 @@
  */
 int print_last_digit(int n)
 {
-	int last_dig = n % 10;
-	if (last_dig(last_dig < 0)
-			last_dig *= -1;
+	int last_digit = n % 10;
+	if (last_digit(last_digit < 0)
+			last_digit *= -1;
 
-	_putchar(last_dig + '0');
+	_putchar(last_digit + '0');
 }
