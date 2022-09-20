1485ddf416a43af5f5ac7fd0ecbae2b1d02fcedf
Feliz <fndayi5@gmail.com>
2022-09-20 06:01:56 +0200

    five question

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*reset_to_98 - resets the value of given integer to 98
+*@n: parameter
+*Return : nothing
+*/
+
+void rev_string(char *s)
+{
+	"Fel" - "leF"
+	char tmp;
+	int i, length1, length2;
+
+	length1=0;
+	length2=0;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2=length1-1
+	for(i=0; i<length1; i++)
+	{
+		"Feli" "ileF"
+		tmp=s[i];
+		s[i]=s[length2]=tmp;
+		length2-=1;
+}
+}
