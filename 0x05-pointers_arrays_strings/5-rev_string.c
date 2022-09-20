7cf9eb1fd9f87d1c29b717112460fe411f4667a3
Feliz <fndayi5@gmail.com>
2022-09-20 06:14:46 +0200

    hotfix on 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,21 +8,20 @@
 
 void rev_string(char *s)
 {
-	"Fel" - "leF"
-	char tmp;
-	int i, length1, length2;
+        char tmp;
+        int i, length1, length2;
 
-	length1 = 0;
-	length2 = 0;
+        length1 = 0;
+        length2 = 0;
 
-	while (s[length1] != '\0')
-		length1++;
+        while (s[length1] != '\0')
+                length1++;
 
-	length2 = length1 - 1
-	for(i = 0; i<length1 / 2; i++)
-	{
-		tmp=s[i];
-		s[i]=s[length2]=tmp;
-		length2- = 1;
+        length2 = length1 - 1
+        for(i = 0; i < length1 / 2; i++)
+        {
+                tmp=s[i];
+                s[i]=s[length2]=tmp;
+                length2- = 1;
 }
 }
