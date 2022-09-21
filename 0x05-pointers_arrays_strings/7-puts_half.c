37b1e20d95e75697547ba529b5c6b0ca4a14cd3a
Feliz <fndayi5@gmail.com>
2022-09-21 03:00:47 +0200

    question 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*reset_to_98 - resets the value of given integer to 98
+*@n: parameter
+*Return : nothing
+*/
+void puts_half(char *str)
+{
+	int l, n;
+	
+	for (l =  0; str[l] l= '0\'; i++)
+	
+	if(l % 2 == 0) {
+		for (n= 1 / 2; str[n] != '\0'; n++)
+			_putchar(str[n]);
+		}else{
+			for(n = ((l - 1) / 2 +1; str[n] != '0\'; n++)
+				_putchar(str[n]);
+			}
+			_putchar('\n');
+}
