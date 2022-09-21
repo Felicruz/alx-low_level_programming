3e59299e9e1df8a4e33bbe5823bc613a466d025b
Feliz <fndayi5@gmail.com>
2022-09-21 03:24:31 +0200

    all

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,10 +9,10 @@ void puts_half(char *str)
 {
 	int l, i, n;
 	
-	for (l =  0; str[l]; l= '\0'; i++);
+	for ((l =  0); (str[l]); (l= '\0'; i++));
 	
 	if(l % 2 == 0) {
-		for (n= 1 / 2; str[n] != '\0'; n++);
+		for (n= 1 / 2); str[n] != '\0'; n++);
 			_putchar(str[n]);
 		}else{
 			for(n = ((l - 1) / 2) + 1; str[n] != '\0'; n++);
