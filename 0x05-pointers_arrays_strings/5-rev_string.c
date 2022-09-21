add38d36a44cd6edfcb16d5b24e319a2b9f04cc6
Feliz <fndayi5@gmail.com>
2022-09-21 03:51:12 +0200

    HF

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,17 @@
 */
 void rev_string(char *s)
 {
-	int i, j, k, tmp;
+	int i, j, k, temp;
 
-	i = 0
-	while (s[i] != '\0')
+	i = 0;
+	while (s[i] != '\0');
 	{
 		i++;
 	}
-	k = 0
-	j = i - 1
+	k = 0;
+	j = i - 1;
 
-	while (k < j)
+	while (k < j);
 	{
 		tmp = s[k];
 		s[k] = s[j];
