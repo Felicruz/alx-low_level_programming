f347158f9b5ff58f02a8915434b467972d775309
Feliz <fndayi5@gmail.com>
2022-09-21 04:24:36 +0200

    HOTFIX

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,23 +7,23 @@
 */
 void rev_string(char *s)
 {
-		int i, j, k, temp;
+	int i, j, k, temp;
 
-		i = 0
+	i = 0
 		
-		while (s[i] != '\0')
-			{
-				i++;
-			}
-		k = 0
-		j = i - 1
+	while (s[i] != '\0')
+		{
+			i++;
+		}
+	k = 0
+	j = i - 1
 
-		while (k < j)
-			{
-				temp = s[k];
-				s[k] = s[j];
-				s[j]  =  temp;
-				k++;
-				j--;
-			}
+	while (k < j)
+		{
+			temp = s[k];
+			s[k] = s[j];
+			s[j]  =  temp;
+			k++;
+			j--;
+		}
 }
