f015fc1f0a123f8465b82b5406c46b8199e7d9ff
Feliz <fndayi5@gmail.com>
2022-09-21 02:39:08 +0200

    pointers and arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,11 +9,12 @@ void rev_string(char *s)
 {
 	int l, i;
 	char ch;
-	
-	for(l = 0; s[l] != '\0'; l++)
-	for (i = 0; i < l /2; i++)
+
+	for(l = 0; s[l]!= '\0'; l++)
+
+	for(i = 0; i < l / 2; i++)
 	{
-		ch = s [l];
+		ch = s[l];
 		s[l] = s[l -1 - i];
 		s[l - 1 - i] = ch;
 	}
