fce1c03b1984862af1cde2ec5d2321534df28a73
Feliz <fndayi5@gmail.com>
2022-09-21 04:49:20 +0200

    New HF

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,22 +4,28 @@
  * puts_half - this function print only the even numbers.
  * @str: is the principal parameter.
  */
+
+/**
+ * puts_half - a function that prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input
+ */
 void puts_half(char *str)
 {
-	int i, len, m;
+	int i, n, len;
 
-	for (len = 0; str[len] != '\0'; len++)
-	{
-	}
+	len = 0;
 
-	if ((len  % 2) == 0)
-		m = (len / 2);
-	else
-		m = (len + 1) / 2;
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
 
-	for (i = m; i < len; i++)
-	{
+	n = (len / 2);
+
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+
+	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
-	}
 	_putchar('\n');
 }
