e182dc19669bffb5331b012b63e6efad4def0cc1
Feliz <fndayi5@gmail.com>
2022-09-21 03:46:36 +0200

    new

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,10 +7,22 @@
 */
 void rev_string(char *s)
 {
-	char s[11] = "Ndayikengur";
-	
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%\n", s);
-	return (0);
+	int i, j, k, tmp;
+
+	i = 0
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0
+	j = i - 1
+
+	while (k < j)
+	{
+		tmp = s[k];
+		s[k] = s[j];
+		s[j]  =  tmp;
+		k++;
+		j--;
+	}
 }
