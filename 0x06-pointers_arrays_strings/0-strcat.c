3d0ae96f5d3e79f24e2d22c0dc91e6b29c3dc7c9
Feliz <fndayi5@gmail.com>
2022-09-22 05:15:38 +0200

    Hot Fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,23 @@
-cat > _putchar.c
-
 #include "main.h"
-#include <unistd.h>
+
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
+* _strncat - Concatenates two strings using at most
+*            an inputted number of bytes from src.
+* @dest: The string to be appended upon.
+* @src: The string to be appended to dest.
+* @n: The number of bytes from src to be appended to dest.
 *
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* Return: A pointer to the resulting string dest.
 */
-int _putchar(char c)
+char *_strncat(char *dest, char *src, int n)
 {
-return (write(1, &c, 1));
+int index = 0, dest_len = 0;
+
+while (dest[index++])
+dest_len++;
+
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+
+return (dest);
 }
